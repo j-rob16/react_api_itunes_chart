3bcecc6dc6547993043136d0f95ee34908ef1a16
James R <jimmer1609@gmail.com>
2022-06-30 15:33:31 +0900

    added album covers to table

--- src/components/ChartTable.js
+++ src/components/ChartTable.js
@@ -4,12 +4,14 @@ import Song from "./Song";
 function ChartTable({songs}) {
 
   const songNodes = songs.map((song, index) => {
-    return <Song song={song} />
+    return <Song song={song} index={index} />
   })
 
   return (
     <table>
       <tr>
+        <th>Position</th>
+        <th>Album Cover</th>
         <th>Title</th>
         <th>Artist</th>
         <th>Genre</th>
--- src/components/Song.js
+++ src/components/Song.js
@@ -1,8 +1,14 @@
 import React from "react";
 
-function Song({song}) {
+function Song({song, index}) {
   return (
     <tr>
+      <td>
+        {index + 1}
+      </td>
+      <td>
+        <img src={song['im:image'][0].label} />
+      </td>
       <td>
         {song['im:name'].label}
       </td>
