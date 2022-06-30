4c91bb533fb16094d62a30548a969a65a4af0f9e
James R <jimmer1609@gmail.com>
2022-06-30 16:52:13 +0900

    extensions completed

--- src/components/ChartHeader.js
+++ src/components/ChartHeader.js
@@ -1,8 +1,24 @@
 import React from "react";
 
-function ChartHeader({title}) {
+function ChartHeader({title, onGenreSelected}) {
+
+  function onChange(evt) {
+    const genre = evt.target.value;
+    onGenreSelected(genre);
+  }
+
   return (
-    <h1>{title}</h1>
+    <>
+      <h1>{title}</h1>
+      <div>
+        <select onChange={onChange}>
+          <option value='all'>All</option>
+          <option value='rock'>Rock</option>
+          <option value='dance'>Dance</option>
+          <option value='country'>Country</option>
+        </select>
+      </div>
+    </>
   )
 }
 
--- src/containers/ChartContainer.js
+++ src/containers/ChartContainer.js
@@ -7,13 +7,33 @@ import '../containers/charts.css';
 function ChartContainer() {
 
   const [songs, setSongs] = useState([]);
+  const [genre, setGenre] = useState('all');
+
+  const genreLink = {
+    all: {
+      title:  'UK top 20',
+      url: 'https://itunes.apple.com/gb/rss/topsongs/limit=20/json'
+    },
+    rock: {
+      title: 'UK top 20 Rock',
+      url: 'https://itunes.apple.com/gb/rss/topsongs/limit=20/genre=21/json'
+    },
+    dance: {
+      title: 'UK top 20 Dance',
+      url: 'https://itunes.apple.com/gb/rss/topsongs/limit=20/genre=17/json'
+    },
+    country: {
+      title: 'UK top 20 country',
+      url: 'https://itunes.apple.com/gb/rss/topsongs/limit=20/genre=6/json'
+    }
+  }
 
   useEffect(() => {
     getChartSongs();
-  }, [])
+  })
 
   const getChartSongs = async function() {
-    const res = await fetch('https://itunes.apple.com/gb/rss/topsongs/limit=20/json');
+    const res = await fetch(genreLink[genre].url);
     const songs = await res.json();
     setSongs(songs.feed.entry);
   }
@@ -21,7 +41,7 @@ function ChartContainer() {
 
   return (
     <main>
-      <ChartHeader title='UK top 20'/>
+      <ChartHeader title={genreLink[genre].title} onGenreSelected={setGenre}/>
       <ChartTable songs={songs}/>
     </main>
   )
